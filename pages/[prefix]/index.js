deff0123c8c4c3a7d7604a8938a24fb944e36c69
0z17 <915208316@qq.com>
2024-11-08 00:03:11 +0800

    support multi passwords for a post

--- pages/[prefix]/index.js
+++ pages/[prefix]/index.js
@@ -38,7 +38,18 @@ const Slug = props => {
       return false
     }
     const encrypt = md5(post?.slug + passInput)
-    if (passInput && encrypt === post?.password) {
+    // if (passInput && encrypt === post?.password) {
+    //   setLock(false)
+    //   // 输入密码存入localStorage，下次自动提交
+    //   localStorage.setItem('password_' + router.asPath, passInput)
+    //   showNotification(locale.COMMON.ARTICLE_UNLOCK_TIPS) // 设置解锁成功提示显示
+    //   return true
+    // }
+
+
+    const passwordArray = Array.isArray(post.password) ? post.password : [post.password];
+
+    if (passInput && passwordArray.includes(encrypt)) {
       setLock(false)
       // 输入密码存入localStorage，下次自动提交
       localStorage.setItem('password_' + router.asPath, passInput)
